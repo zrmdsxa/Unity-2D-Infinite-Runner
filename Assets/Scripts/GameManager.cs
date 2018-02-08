98dfabcc40e505e2e3d5ca9689d6774ece4d198d
Nue <undefinedhoujuunue@gmail.com>
2018-02-08 01:53:11 -0500

    build project on unity 2017.3.0f3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,158 +3,179 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
 
-	
-
-	public float m_speedIncreaseRate;
-
-	public Text m_scoreUIText;
-	public Text m_highscoreUIText;
-
-	public GameObject player;
-	public GameObject roadroller;
-	private float m_score;
-	private float m_gameSpeed;
-
-	public GameObject[] m_prefabSections;
-
-	public float[] m_prefabSectionsLength;
-
-	public float GameSpeed{get {return m_gameSpeed;}}
-
-	private static GameManager m_instance = null;
-	public static GameManager Instance{get{return m_instance;}}
-
-	private int m_currentPrefab = 2;
-	private float m_currentPrefabPos = 0;
-
-	private int m_numPrefabs; // 0 sm, 1 med, 2 long
 
 
-	//obstacles
+    public float m_speedIncreaseRate;
 
-	public float nextObstacleMin = 1.0f;
-	public float nextObstacleMax = 5.0f;
-	public float nextObstacleTimer;
+    public Text m_scoreUIText;
+    public Text m_highscoreUIText;
 
-	public GameObject[] m_prefabObstacles;
+    public GameObject player;
+    public GameObject roadroller;
+    private float m_score;
+    private float m_gameSpeed;
 
-	// Use this for initialization
-	void Awake(){
-		if (m_instance != null && m_instance != this){
-			Destroy(gameObject);
-			return;
-		}
-		else{
-			m_instance = this;
-		}
-		DontDestroyOnLoad(gameObject);
+    public GameObject[] m_prefabSections;
 
-		m_numPrefabs = m_prefabSections.Length;
-	}
+    public float[] m_prefabSectionsLength;
 
-	void Start () {
-		m_score = 0f;
-		m_speedIncreaseRate = 0.1f;
-		m_gameSpeed = 1f;
+    public float GameSpeed { get { return m_gameSpeed; } }
 
-		m_highscoreUIText.text = "Highscore:"+PlayerPrefs.GetFloat("highscore",0).ToString("0000");
+    private static GameManager m_instance = null;
+    public static GameManager Instance { get { return m_instance; } }
 
-		AddSection();
+    private int m_currentPrefab = 2;
+    private float m_currentPrefabPos = 0;
 
-		nextObstacleTimer = Random.Range(nextObstacleMin,nextObstacleMax);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (StateManager.Instance.m_currentState == StateManager.m_states.PLAY)
-		m_gameSpeed += m_speedIncreaseRate * Time.deltaTime;
-		m_score += Time.deltaTime;
+    private int m_numPrefabs; // 0 sm, 1 med, 2 long
 
-		nextObstacleTimer -= Time.deltaTime;
-		if (nextObstacleTimer <= 0.0f){
-			CreateObstacle();
-		}
 
-		if (m_gameSpeed > 4.0f){
-			m_gameSpeed = 4.0f;
-		}
+    //obstacles
 
-		//TODO: remove debugging information
-		m_scoreUIText.text = "Score: " + Mathf.Floor(m_score);
-	}
+    public float nextObstacleMin = 0.5f;
+    public float nextObstacleMax = 4.0f;
+    public float nextObstacleTimer;
 
-	public void AddSection(){
-		int randomSection = (int)Random.Range(0,m_numPrefabs);
+    public GameObject[] m_prefabObstacles;
 
-		CreateSection(randomSection);
-	}
+    // Use this for initialization
+    void Awake()
+    {
+        if (m_instance != null && m_instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            m_instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
 
-	private void CreateSection(int s){
-		Vector3 pos = Vector3.zero;
-		pos.z = -1;
-		pos.x = m_prefabSectionsLength[m_currentPrefab] + m_currentPrefabPos;
-		GameObject tmp = Instantiate(m_prefabSections[s], pos, Quaternion.identity) as GameObject;
-		tmp.name = "Section - " +s;
-		m_currentPrefab = s;
-		m_currentPrefabPos = pos.x;
-	}
+        m_numPrefabs = m_prefabSections.Length;
+    }
 
-	public void RestartLevel(){
-		m_gameSpeed = 1.0f;
-		m_score = 0;
-		player.transform.position = new Vector3(0.5f,0.01f,-1f);
-		roadroller.transform.position = new Vector3(0.5f,1.253f,-3.053f);
-		player.GetComponent<PlayerController>().Restart();
+    void Start()
+    {
+        m_score = 0f;
+        m_speedIncreaseRate = 0.1f;
+        m_gameSpeed = 1f;
 
-		m_currentPrefabPos = -10.0f;
-		GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
-		foreach(GameObject go in obstacles){
-			Destroy(go);
-		}
-		GameObject[] grounds = GameObject.FindGameObjectsWithTag("Ground");
-		foreach(GameObject go in grounds){
-			Destroy(go);
-		}
+        m_highscoreUIText.text = "Highscore:" + PlayerPrefs.GetFloat("highscore", 0).ToString("0000");
 
-		CreateSection(2);
-		CreateSection(2);
-		CreateSection(2);
-		
-	}
+        AddSection();
 
-	public void RestartSpeed(){
-		m_gameSpeed = 1f;
-	}
+        nextObstacleTimer = Random.Range(nextObstacleMin, nextObstacleMax);
+    }
 
-	public int GetScore(){
-		return (int)Mathf.Floor(m_score);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (StateManager.Instance.m_currentState == StateManager.m_states.PLAY)
+        {
+            m_gameSpeed += m_speedIncreaseRate * Time.deltaTime;
+            m_score += Time.deltaTime;
 
-	public void AddBonus(int pointsToAdd){
-		m_score += pointsToAdd;
-	}
-	void CreateObstacle(){
-//		Debug.Log("create obstacle");
-		nextObstacleTimer = Random.Range(nextObstacleMin,nextObstacleMax);
-		Vector3 pos = Vector3.zero;
-		
-		pos.x = m_currentPrefabPos;
-		pos.y = 0.25f;
-		pos.z = -1.0f;
-		
+            nextObstacleTimer -= Time.deltaTime;
 
-		GameObject tmp = Instantiate(m_prefabObstacles[Random.Range(0,m_prefabObstacles.Length)],pos,Quaternion.identity) as GameObject;
-		
-	}
+            if (nextObstacleTimer <= 0.0f)
+            {
+                CreateObstacle();
+            }
 
-	public void PlayerDied(){
-		PlayerPrefs.SetFloat("highscore",m_score);
-		m_highscoreUIText.text = "Highscore:"+PlayerPrefs.GetFloat("highscore",0).ToString("0000");
-		StateManager.Instance.GameOver();
+            if (m_gameSpeed > 4.0f)
+            {
+                m_gameSpeed = 4.0f;
+            }
 
-		
-	}
+            //TODO: remove debugging information
+            m_scoreUIText.text = "Score: " + Mathf.Floor(m_score);
+        }
+    }
+
+    public void AddSection()
+    {
+        int randomSection = (int)Random.Range(0, m_numPrefabs);
+
+        CreateSection(randomSection);
+    }
+
+    private void CreateSection(int s)
+    {
+        Vector3 pos = Vector3.zero;
+        pos.z = -1;
+        pos.x = m_prefabSectionsLength[m_currentPrefab] + m_currentPrefabPos;
+        GameObject tmp = Instantiate(m_prefabSections[s], pos, Quaternion.identity) as GameObject;
+        tmp.name = "Section - " + s;
+        m_currentPrefab = s;
+        m_currentPrefabPos = pos.x;
+    }
+
+    public void RestartLevel()
+    {
+        m_gameSpeed = 1.0f;
+        m_score = 0;
+        player.transform.position = new Vector3(0.5f, 0.01f, -1f);
+        roadroller.transform.position = new Vector3(0.5f, 1.253f, -3.053f);
+        player.GetComponent<PlayerController>().Restart();
+
+        m_currentPrefabPos = -10.0f;
+        GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+        foreach (GameObject go in obstacles)
+        {
+            Destroy(go);
+        }
+        GameObject[] grounds = GameObject.FindGameObjectsWithTag("Ground");
+        foreach (GameObject go in grounds)
+        {
+            Destroy(go);
+        }
+
+        CreateSection(2);
+        CreateSection(2);
+        CreateSection(2);
+
+    }
+
+    public void RestartSpeed()
+    {
+        m_gameSpeed = 1f;
+    }
+
+    public int GetScore()
+    {
+        return (int)Mathf.Floor(m_score);
+    }
+
+    public void AddBonus(int pointsToAdd)
+    {
+        m_score += pointsToAdd;
+    }
+    void CreateObstacle()
+    {
+        //		Debug.Log("create obstacle");
+        nextObstacleTimer = Random.Range(nextObstacleMin, nextObstacleMax);
+        Vector3 pos = Vector3.zero;
+
+        pos.x = m_currentPrefabPos;
+        pos.y = 0.25f;
+        pos.z = -1.0f;
+
+
+        GameObject tmp = Instantiate(m_prefabObstacles[Random.Range(0, m_prefabObstacles.Length)], pos, Quaternion.identity) as GameObject;
+
+    }
+
+    public void PlayerDied()
+    {
+        PlayerPrefs.SetFloat("highscore", m_score);
+        m_highscoreUIText.text = "Highscore:" + PlayerPrefs.GetFloat("highscore", 0).ToString("0000");
+        StateManager.Instance.GameOver();
+
+
+    }
 }
